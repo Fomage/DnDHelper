2dab0062dd4504c885cb20ed77b32d80761feb75
Pierre Lataillade <pierre.lataillade@gmail.com>
2015-07-08 19:10:39 +0200

    Putting in the MainWindow and Launcher

--- DnDHelper/src/gui/MainWindow.java
+++ DnDHelper/src/gui/MainWindow.java
@@ -1,18 +1,34 @@
 package gui;
 
+
+
 import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MainWindow extends JFrame  {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2740437090361841747L;
+
 	public MainWindow(){
 		JPanel mainPanel = new JPanel();
 		mainPanel.setBackground(Color.white);
 		mainPanel.setLayout(null); // MODIFIER LE LAYOUT
 		this.setContentPane(mainPanel);
 		
+		this.setTitle("DnDHelper");
+		this.setSize(new Dimension(300,800));
+		this.setLocation(0, 0);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setAlwaysOnTop(true);
 		
 		
+		//this.pack();
+		this.setVisible(true);
 	}
 }
