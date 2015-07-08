5feebc0ccfef164ae34c206f09b9ca6376bd4613
Pierre Lataillade <pierre.lataillade@gmail.com>
2015-07-08 22:53:05 +0200

    The beggining of the end for the gui

--- DnDHelper/src/gui/CreaturePanel.java
+++ DnDHelper/src/gui/CreaturePanel.java
@@ -4,8 +4,11 @@ import javax.swing.JPanel;
 import javax.swing.border.BevelBorder;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
+
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Color;
+
 import javax.swing.JButton;
 
 public class CreaturePanel extends JPanel {
@@ -17,9 +20,11 @@ public class CreaturePanel extends JPanel {
 		setBackground(Color.WHITE);
 		setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		setLayout(null);
+		this.setPreferredSize(new Dimension(300, 100));
+		//this.setMinimumSize(new Dimension(300, 100));
 		
 		JPanel MainPanel = new JPanel();
-		MainPanel.setBounds(0, 0, 250, 150);
+		MainPanel.setBounds(0, 0, 250, 100);
 		add(MainPanel);
 		MainPanel.setLayout(null);
 		
@@ -40,7 +45,7 @@ public class CreaturePanel extends JPanel {
 		MainPanel.add(btnInventory);
 		
 		JPanel BuffPanel = new JPanel();
-		BuffPanel.setBounds(10, 46, 230, 93);
+		BuffPanel.setBounds(10, 46, 219, 46);
 		MainPanel.add(BuffPanel);
 		
 		JPanel SelectPanel = new JPanel();
@@ -49,8 +54,8 @@ public class CreaturePanel extends JPanel {
 		SelectPanel.setLayout(null);
 		
 		JButton btnSelect = new JButton("Select");
-		btnSelect.setBounds(0, 0, 50, 150);
+		btnSelect.setBounds(0, 0, 50, 100);
 		SelectPanel.add(btnSelect);
-
+		
 	}
 }
--- DnDHelper/src/gui/MainWindow.java
+++ DnDHelper/src/gui/MainWindow.java
@@ -1,12 +1,27 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
 import java.awt.Color;
+import java.awt.GridBagLayout;
+
+import javax.swing.BoxLayout;
+
+import java.awt.GridLayout;
+import javax.swing.JRadioButton;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import javax.swing.JCheckBox;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
 
 public class MainWindow extends JFrame {
 
@@ -32,15 +47,53 @@ public class MainWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public MainWindow() {
+		setResizable(false);
 		setTitle("DnDHelper");
 		setAlwaysOnTop(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 294, 729);
+		MainWindow main = this;
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		contentPane.add(new CreaturePanel());
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel creatures = new JPanel();
+		contentPane.add(creatures, BorderLayout.NORTH);
+		creatures.setLayout(new BoxLayout(creatures, BoxLayout.PAGE_AXIS));
+		
+		JPanel test1 = new CreaturePanel();
+		creatures.add(test1);
+		
+		JPanel test2 = new CreaturePanel();
+		creatures.add(test2);
+		
+		JPanel bot = new JPanel();
+		bot.setPreferredSize(new Dimension(300, 40));
+		contentPane.add(bot,BorderLayout.SOUTH);
+		bot.setLayout(new BorderLayout(0, 0));
+		
+		JButton btnAddcreature = new JButton("Add Creature");
+		bot.add(btnAddcreature,BorderLayout.WEST);
+		
+		JCheckBox chckbxResizable = new JCheckBox("Resizable");
+		chckbxResizable.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(main.isResizable()){
+					
+					main.setResizable(false);
+					contentPane.validate();
+					main.pack();
+				}
+				else{
+					main.setResizable(true);
+				}
+			}
+		});
+		bot.add(chckbxResizable,BorderLayout.EAST);
+		
+		contentPane.validate();
+		this.pack();
 	}
 
 }
